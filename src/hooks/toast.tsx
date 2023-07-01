95154302617f523fe0e2a691848759f86ac25076
Junior5801 <junior_silvapereira@hotmail.com>
2023-07-01 19:23:33 -0300

    fix: add toast

--- src/hooks/toast.tsx
+++ src/hooks/toast.tsx
@@ -1,9 +1,17 @@
-import { createContext, useContext, useCallback } from 'react'
+import { createContext, useContext, useCallback, useState } from 'react'
+import { v4 as uuid } from 'uuid'
 import { ToastContainer } from '../components/ToastContainer'
 
+export interface IToastMessage {
+  id: string
+  type?: 'success' | 'info' | 'error'
+  title: string
+  description?: string
+}
+
 interface IToastContextData {
-  addToast(): void
-  removeToast(): void
+  addToast(message: Omit<IToastMessage, 'id'>): void
+  removeToast(id: string): void
 }
 
 interface IProps {
@@ -15,17 +23,31 @@ export const ToastContext = createContext<IToastContextData>(
 )
 
 export const ToastProvider: React.FC<IProps> = ({ children }) => {
-  const addToast = useCallback(() => {
-    console.log('addToast')
-  }, [])
-  const removeToast = useCallback(() => {
-    console.log('remove')
+  const [messages, setMessages] = useState<IToastMessage[]>([])
+
+  const addToast = useCallback(
+    ({ type, title, description }: Omit<IToastMessage, 'id'>) => {
+      const id = uuid()
+      const toast = {
+        id,
+        type,
+        title,
+        description,
+      }
+
+      setMessages(oldMessages => [...oldMessages, toast])
+    },
+    [],
+  )
+
+  const removeToast = useCallback((id: string) => {
+    setMessages(state => state.filter(message => message.id !== id))
   }, [])
 
   return (
     <ToastContext.Provider value={{ addToast, removeToast }}>
       {children}
-      <ToastContainer />
+      <ToastContainer messages={messages} />
     </ToastContext.Provider>
   )
 }
