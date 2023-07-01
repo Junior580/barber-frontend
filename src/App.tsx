578a9a2b1551be723b06170566f5534e8a4a3a5a
Junior5801 <junior_silvapereira@hotmail.com>
2023-06-30 23:20:25 -0300

    fix: add toast + styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import GlobalStyle from './styles/global'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './hooks/AuthContext'
 import { AppRoutes } from './routes'
+import { ToastContainer } from './components/ToastContainer'
 
 export const App: React.FC = () => (
   <BrowserRouter>
     <AuthProvider>
       <AppRoutes />
+      <ToastContainer />
     </AuthProvider>
     <GlobalStyle />
   </BrowserRouter>
