5d37d934e9616c35bbc7db7559ab8aca1abefc2c
Junior da Silva Pereira <junior_silvapereira@hotmail.com>
2024-01-27 02:38:14 +1100

    refactor

--- src/pages/App/App.tsx
+++ src/pages/App/App.tsx
@@ -1,6 +1,4 @@
-import GlobalStyle from '../../styles/global'
 import { createBrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { SignIn } from '../Home/SignIn'
 import { SignUp } from '../Home/SignUp'
@@ -8,26 +6,6 @@ import { Admin } from '../Admin'
 
 import { ForgotPassword } from '../Home/ForgotPassword'
 import { ResetPassword } from '../Home/ResetPassword'
-import { ToastProvider } from '../../hooks/toast'
-
-// export const App: React.FC = () => (
-//   <BrowserRouter>
-//     <QueryClientProvider client={queryClient}>
-//       <ToastProvider>
-//         <Routes>
-//           <Route path="/" element={<SignIn />} />
-//           <Route path="/signup" element={<SignUp />} />
-//           <Route path="/forgot-password" element={<ForgotPassword />} />
-//           <Route path="/reset-password/*" element={<ResetPassword />} />
-
-//           <Route path="/dashboard/*" element={<Admin />} />
-//           <Route path="*" element={<Navigate to="/" />} />
-//         </Routes>
-//       </ToastProvider>
-//     </QueryClientProvider>
-//     <GlobalStyle />
-//   </BrowserRouter>
-// )
 
 export const router = createBrowserRouter([
   {
